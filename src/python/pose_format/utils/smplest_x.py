bc72d10d7b004d86b990c4c07d3ff8cdce40f88c
GerrySant <gerard.santmuniesa@uzh.ch>
2026-02-05 11:58:59 +0100

    [UPD] Duplication of wrist keypoints has been added to represent the pose correctly. New limbs have been added to the hands.

--- src/python/pose_format/utils/smplest_x.py
+++ src/python/pose_format/utils/smplest_x.py
@@ -6,122 +6,186 @@ from ..pose import Pose
 from ..pose_header import PoseHeader, PoseHeaderComponent, PoseHeaderDimensions
 from pose_format.utils.openpose import hand_colors
 
+SMPLX_BODY_POINTS = [
+    "Pelvis",
+    "L_Hip", "R_Hip",
+    "L_Knee", "R_Knee",
+    "L_Ankle", "R_Ankle",
+    "Neck",
+    "L_Shoulder", "R_Shoulder",
+    "L_Elbow", "R_Elbow",
+    "L_Wrist", "R_Wrist",
+    "L_Big_toe", "L_Small_toe", "L_Heel",
+    "R_Big_toe", "R_Small_toe", "R_Heel",
+    "L_Ear", "R_Ear",
+    "L_Eye", "R_Eye",
+    "Nose",
+]
+
+SMPLX_LEFT_HAND_POINTS = [
+    "L_Thumb_1", "L_Thumb_2", "L_Thumb_3", "L_Thumb_4",
+    "L_Index_1", "L_Index_2", "L_Index_3", "L_Index_4",
+    "L_Middle_1", "L_Middle_2", "L_Middle_3", "L_Middle_4",
+    "L_Ring_1", "L_Ring_2", "L_Ring_3", "L_Ring_4",
+    "L_Pinky_1", "L_Pinky_2", "L_Pinky_3", "L_Pinky_4",
+]
+
+EXTENDED_SMPLX_LEFT_HAND_POINTS = (
+    SMPLX_LEFT_HAND_POINTS + ["L_Wrist_Hand"]
+)
+
+SMPLX_RIGHT_HAND_POINTS = [
+    "R_Thumb_1", "R_Thumb_2", "R_Thumb_3", "R_Thumb_4",
+    "R_Index_1", "R_Index_2", "R_Index_3", "R_Index_4",
+    "R_Middle_1", "R_Middle_2", "R_Middle_3", "R_Middle_4",
+    "R_Ring_1", "R_Ring_2", "R_Ring_3", "R_Ring_4",
+    "R_Pinky_1", "R_Pinky_2", "R_Pinky_3", "R_Pinky_4",
+]
+
+EXTENDED_SMPLX_RIGHT_HAND_POINTS = (
+    SMPLX_RIGHT_HAND_POINTS + ["R_Wrist_Hand"]
+)
+
+SMPLX_FACE_POINTS = [f"Face_{i}" for i in range(1, 73)]
+
+SMPLX_JOINT_NAMES = (
+    tuple(SMPLX_BODY_POINTS)
+    + tuple(SMPLX_LEFT_HAND_POINTS)
+    + tuple(SMPLX_RIGHT_HAND_POINTS)
+    + tuple(SMPLX_FACE_POINTS)
+)
+
+assert len(SMPLX_JOINT_NAMES) == 137
+
+EXTENDED_SMPLX_JOINT_NAMES = (
+    tuple(SMPLX_BODY_POINTS)
+    + tuple(EXTENDED_SMPLX_LEFT_HAND_POINTS)
+    + tuple(EXTENDED_SMPLX_RIGHT_HAND_POINTS)
+    + tuple(SMPLX_FACE_POINTS)
+)
+
+assert len(EXTENDED_SMPLX_JOINT_NAMES) == 139
+
+orig_name_to_idx = {
+    name: i for i, name in enumerate(SMPLX_JOINT_NAMES)
+}
+
+final_name_to_idx = {
+    name: i for i, name in enumerate(EXTENDED_SMPLX_JOINT_NAMES)
+}
+
+BODY_LIMBS_NAMES = [
+    ("Pelvis", "L_Hip"),
+    ("Pelvis", "R_Hip"),
+    ("L_Hip", "L_Knee"),
+    ("R_Hip", "R_Knee"),
+    ("L_Knee", "L_Ankle"),
+    ("R_Knee", "R_Ankle"),
+
+    ("Pelvis", "Neck"),
+    ("Neck", "L_Shoulder"),
+    ("Neck", "R_Shoulder"),
+    ("L_Shoulder", "L_Elbow"),
+    ("R_Shoulder", "R_Elbow"),
+    ("L_Elbow", "L_Wrist"),
+    ("R_Elbow", "R_Wrist"),
+
+    ("L_Ankle", "L_Big_toe"),
+    ("L_Ankle", "L_Small_toe"),
+    ("L_Ankle", "L_Heel"),
+    ("R_Ankle", "R_Big_toe"),
+    ("R_Ankle", "R_Small_toe"),
+    ("R_Ankle", "R_Heel"),
+
+    ("Neck", "Nose"),
+    ("Nose", "L_Eye"),
+    ("Nose", "R_Eye"),
+    ("L_Eye", "L_Ear"),
+    ("R_Eye", "R_Ear"),
+]
+LEFT_HAND_LIMBS_NAMES = [
+    ("L_Wrist_Hand", "L_Thumb_1"), ("L_Wrist_Hand", "L_Index_1"), ("L_Wrist_Hand", "L_Middle_1"),
+    ("L_Wrist_Hand", "L_Ring_1"), ("L_Wrist_Hand", "L_Pinky_1"),
+    ("L_Thumb_1", "L_Thumb_2"), ("L_Thumb_2", "L_Thumb_3"), ("L_Thumb_3", "L_Thumb_4"),
+    ("L_Index_1", "L_Index_2"), ("L_Index_2", "L_Index_3"), ("L_Index_3", "L_Index_4"),
+    ("L_Middle_1", "L_Middle_2"), ("L_Middle_2", "L_Middle_3"), ("L_Middle_3", "L_Middle_4"),
+    ("L_Ring_1", "L_Ring_2"), ("L_Ring_2", "L_Ring_3"), ("L_Ring_3", "L_Ring_4"),
+    ("L_Pinky_1", "L_Pinky_2"), ("L_Pinky_2", "L_Pinky_3"), ("L_Pinky_3", "L_Pinky_4"),
+    ("L_Index_1", "L_Middle_1"), ("L_Middle_1", "L_Ring_1"), ("L_Ring_1", "L_Pinky_1")
+]
+
+RIGHT_HAND_LIMBS_NAMES = [
+    ("R_Wrist_Hand", "R_Thumb_1"), ("R_Wrist_Hand", "R_Index_1"), ("R_Wrist_Hand", "R_Middle_1"),
+    ("R_Wrist_Hand", "R_Ring_1"), ("R_Wrist_Hand", "R_Pinky_1"),
+    ("R_Thumb_1", "R_Thumb_2"), ("R_Thumb_2", "R_Thumb_3"), ("R_Thumb_3", "R_Thumb_4"),
+    ("R_Index_1", "R_Index_2"), ("R_Index_2", "R_Index_3"), ("R_Index_3", "R_Index_4"),
+    ("R_Middle_1", "R_Middle_2"), ("R_Middle_2", "R_Middle_3"), ("R_Middle_3", "R_Middle_4"),
+    ("R_Ring_1", "R_Ring_2"), ("R_Ring_2", "R_Ring_3"), ("R_Ring_3", "R_Ring_4"),
+    ("R_Pinky_1", "R_Pinky_2"), ("R_Pinky_2", "R_Pinky_3"), ("R_Pinky_3", "R_Pinky_4"),
+    ("R_Index_1", "R_Middle_1"), ("R_Middle_1", "R_Ring_1"), ("R_Ring_1", "R_Pinky_1")
+]
+
+def build_smplx_with_hand_wrists(joints, conf):
+    """
+    Rebuild SMPL-X joints following BODY → LEFT_HAND → RIGHT_HAND → FACE order,
+    duplicating wrists as hand roots.
+    """
+    K_final = len(EXTENDED_SMPLX_JOINT_NAMES)
+
+    xy_ext = np.zeros((K_final, 2), dtype=joints.dtype)
+    conf_ext = np.zeros((K_final,), dtype=conf.dtype)
+
+    for i, name in enumerate(EXTENDED_SMPLX_JOINT_NAMES):
+        if name == "L_Wrist_Hand":
+            src = orig_name_to_idx["L_Wrist"]
+            print(f"L_Wrist src: {src}")
+            print(f"L_Wrist_Hand i: {i}")
+            print(f"xy_ext[{i}] = joints[{src}]")
+        elif name == "R_Wrist_Hand":
+            src = orig_name_to_idx["R_Wrist"]
+            print(f"R_Wrist src: {src}")
+            print(f"R_Wrist_Hand i: {i}")
+            print(f"xy_ext[{i}] = joints[{src}]")
+        else:
+            src = orig_name_to_idx[name]
+
+        xy_ext[i] = joints[src]
+        conf_ext[i] = conf[src]
+
+    return xy_ext, conf_ext
+
+
 def smplx_components():
 
     def map_limbs(points, limbs):
         index_map = {name: idx for idx, name in enumerate(points)}
         return [(index_map[a], index_map[b]) for a, b in limbs]
 
-    SMPLX_JOINT_NAMES = (
-        # --- Body (25) ---
-        "Pelvis",
-        "L_Hip", "R_Hip",
-        "L_Knee", "R_Knee",
-        "L_Ankle", "R_Ankle",
-        "Neck",
-        "L_Shoulder", "R_Shoulder",
-        "L_Elbow", "R_Elbow",
-        "L_Wrist", "R_Wrist",
-        "L_Big_toe", "L_Small_toe", "L_Heel",
-        "R_Big_toe", "R_Small_toe", "R_Heel",
-        "L_Ear", "R_Ear",
-        "L_Eye", "R_Eye",
-        "Nose",
-
-        # --- Left hand (20) ---
-        "L_Thumb_1", "L_Thumb_2", "L_Thumb_3", "L_Thumb_4",
-        "L_Index_1", "L_Index_2", "L_Index_3", "L_Index_4",
-        "L_Middle_1", "L_Middle_2", "L_Middle_3", "L_Middle_4",
-        "L_Ring_1", "L_Ring_2", "L_Ring_3", "L_Ring_4",
-        "L_Pinky_1", "L_Pinky_2", "L_Pinky_3", "L_Pinky_4",
-
-        # --- Right hand (20) ---
-        "R_Thumb_1", "R_Thumb_2", "R_Thumb_3", "R_Thumb_4",
-        "R_Index_1", "R_Index_2", "R_Index_3", "R_Index_4",
-        "R_Middle_1", "R_Middle_2", "R_Middle_3", "R_Middle_4",
-        "R_Ring_1", "R_Ring_2", "R_Ring_3", "R_Ring_4",
-        "R_Pinky_1", "R_Pinky_2", "R_Pinky_3", "R_Pinky_4",
-
-        # --- Face (72) ---
-        *[f"Face_{i}" for i in range(1, 73)],
-    )
-    assert len(SMPLX_JOINT_NAMES) == 137
-
-    BODY_LIMBS_NAMES = [
-        ("Pelvis", "L_Hip"),
-        ("Pelvis", "R_Hip"),
-        ("L_Hip", "L_Knee"),
-        ("R_Hip", "R_Knee"),
-        ("L_Knee", "L_Ankle"),
-        ("R_Knee", "R_Ankle"),
-
-        ("Pelvis", "Neck"),
-        ("Neck", "L_Shoulder"),
-        ("Neck", "R_Shoulder"),
-        ("L_Shoulder", "L_Elbow"),
-        ("R_Shoulder", "R_Elbow"),
-        ("L_Elbow", "L_Wrist"),
-        ("R_Elbow", "R_Wrist"),
-
-        ("L_Ankle", "L_Big_toe"),
-        ("L_Ankle", "L_Small_toe"),
-        ("L_Ankle", "L_Heel"),
-        ("R_Ankle", "R_Big_toe"),
-        ("R_Ankle", "R_Small_toe"),
-        ("R_Ankle", "R_Heel"),
-
-        ("Neck", "Nose"),
-        ("Nose", "L_Eye"),
-        ("Nose", "R_Eye"),
-        ("L_Eye", "L_Ear"),
-        ("R_Eye", "R_Ear"),
-    ]
-    LEFT_HAND_LIMBS_NAMES = [
-        ("L_Thumb_1", "L_Thumb_2"), ("L_Thumb_2", "L_Thumb_3"), ("L_Thumb_3", "L_Thumb_4"),
-        ("L_Index_1", "L_Index_2"), ("L_Index_2", "L_Index_3"), ("L_Index_3", "L_Index_4"),
-        ("L_Middle_1", "L_Middle_2"), ("L_Middle_2", "L_Middle_3"), ("L_Middle_3", "L_Middle_4"),
-        ("L_Ring_1", "L_Ring_2"), ("L_Ring_2", "L_Ring_3"), ("L_Ring_3", "L_Ring_4"),
-        ("L_Pinky_1", "L_Pinky_2"), ("L_Pinky_2", "L_Pinky_3"), ("L_Pinky_3", "L_Pinky_4"),
-    ]
-    RIGHT_HAND_LIMBS_NAMES = [
-        ("R_Thumb_1", "R_Thumb_2"), ("R_Thumb_2", "R_Thumb_3"), ("R_Thumb_3", "R_Thumb_4"),
-        ("R_Index_1", "R_Index_2"), ("R_Index_2", "R_Index_3"), ("R_Index_3", "R_Index_4"),
-        ("R_Middle_1", "R_Middle_2"), ("R_Middle_2", "R_Middle_3"), ("R_Middle_3", "R_Middle_4"),
-        ("R_Ring_1", "R_Ring_2"), ("R_Ring_2", "R_Ring_3"), ("R_Ring_3", "R_Ring_4"),
-        ("R_Pinky_1", "R_Pinky_2"), ("R_Pinky_2", "R_Pinky_3"), ("R_Pinky_3", "R_Pinky_4"),
-    ]
-
-
-    BODY_POINTS = SMPLX_JOINT_NAMES[0:25]
-    LEFT_HAND_POINTS = SMPLX_JOINT_NAMES[25:45]
-    RIGHT_HAND_POINTS = SMPLX_JOINT_NAMES[45:65]
-    FACE_POINTS = SMPLX_JOINT_NAMES[65:137]
-
     return [
         PoseHeaderComponent(
             name="BODY",
-            points=BODY_POINTS,
-            limbs=map_limbs(BODY_POINTS, BODY_LIMBS_NAMES),
+            points=SMPLX_BODY_POINTS,
+            limbs=map_limbs(SMPLX_BODY_POINTS, BODY_LIMBS_NAMES),
             colors=[(0, 255, 0)],
             point_format="XYC",
         ),
         PoseHeaderComponent(
             name="LEFT_HAND",
-            points=LEFT_HAND_POINTS,
-            limbs=map_limbs(LEFT_HAND_POINTS, LEFT_HAND_LIMBS_NAMES),
+            points=EXTENDED_SMPLX_LEFT_HAND_POINTS,
+            limbs=map_limbs(EXTENDED_SMPLX_LEFT_HAND_POINTS, LEFT_HAND_LIMBS_NAMES),
             colors=[(0, 255, 255)],
             point_format="XYC",
         ),
         PoseHeaderComponent(
             name="RIGHT_HAND",
-            points=RIGHT_HAND_POINTS,
-            limbs=map_limbs(RIGHT_HAND_POINTS, RIGHT_HAND_LIMBS_NAMES),
+            points=EXTENDED_SMPLX_RIGHT_HAND_POINTS,
+            limbs=map_limbs(EXTENDED_SMPLX_RIGHT_HAND_POINTS, RIGHT_HAND_LIMBS_NAMES),
             colors=[(255, 128, 0)],
             point_format="XYC",
         ),
         PoseHeaderComponent(
             name="FACE",
-            points=FACE_POINTS,
+            points=SMPLX_FACE_POINTS,
             limbs=[],  # face mesh too dense → usually omitted
             colors=[(255, 255, 255)],
             point_format="XYC",
@@ -143,6 +207,11 @@ def load_smplestx_pose(
 
     frames = data["frames"]
     num_joints = data["num_joints"]
+    num_joints = len(EXTENDED_SMPLX_JOINT_NAMES)
+
+    fps = data.get("fps", fps)
+    width = data.get("width", width)
+    height = data.get("height", height)
 
     # Canvas size
     json_w, json_h = frames[0]["image_size"]
@@ -196,8 +265,18 @@ def load_smplestx_pose(
             joints_norm[:, 0] = (joints_img[:, 0] - min_xy[0]) * (usable_width / bw) + offset_x
             joints_norm[:, 1] = (joints_img[:, 1] - min_xy[1]) * (usable_height / bh) + offset_y
 
-            persons_data.append(joints_norm)
-            persons_conf.append(np.ones((num_joints,), dtype=np.float32))
+            # persons_data.append(joints_norm)
+            # persons_conf.append(np.ones((num_joints,), dtype=np.float32))
+
+            # confidence for original SMPL-X joints (137)
+            conf = np.ones((joints_norm.shape[0],), dtype=np.float32)
+
+            # rebuild joints following BODY → LEFT_HAND → RIGHT_HAND → FACE order
+            # and duplicate wrists inside hand components
+            joints_ext, conf_ext = build_smplx_with_hand_wrists(joints_norm, conf)
+
+            persons_data.append(joints_ext)
+            persons_conf.append(conf_ext)
 
         all_data.append(persons_data)
         all_conf.append(persons_conf)
